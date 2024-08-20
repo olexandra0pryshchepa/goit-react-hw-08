caef00032628bc92fa05ceed6fc07180abc8e8a4
olexandra0pryshchepa <130248875+olexandra0pryshchepa@users.noreply.github.com>
2024-08-20 13:43:28 +0300

    commit

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,8 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "../../redux/contacts/operations";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
 
 export default function ContactsPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div>
       <h1>Contacts</h1>
@@ -12,5 +21,3 @@ export default function ContactsPage() {
     </div>
   );
 };
-
-
--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,8 +1,15 @@
+import { createSelector } from '@reduxjs/toolkit';
+
 export const selectNameFilter = state => state.filters.name;
 
-export const selectFilter = state => {
-  const filter = state.filters.name.toLowerCase();
-  return state.contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter)
-  );
-};
+export const selectContacts = state => state.contacts.items;
+
+export const selectFilter = createSelector(
+  [selectContacts, selectNameFilter],
+  (contacts, filter) => {
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.items.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  }
+);
